6ac935a9b31ff97f340e5c9b2f2b95049fd7a810
IghorDrummond <95970657+IghorDrummond@users.noreply.github.com>
2023-04-04 14:59:37 -0300

    PROJETO SIMULADOR UCP - V1.2.6
    Foi alterado o fonte Program.js para futuras edições.

---
+++ program.js
@@ -0,0 +1,23 @@
+
+/*
+=====================================================================================================================
+    Fonte: program.js 
+    Motivo: Responsavel pela logica e funcionamento do html 'index.html' ao qual simula uma movimentação de registro.
+    Data: 04/04/2023
+    Programador: Ighor Drummond, Hikelmi Santos
+=====================================================================================================================
+*/
+/*
+===================================
+    Declaração das Variaveis
+===================================
+*/
+    var desconsole = "";
+    var selecao1 = "";
+    var registro = {};
+
+    window.alert("Versão 1.2.6 - Alpha");
+    desconsole = "Seja bem-vindo ao Console da pagina! Este projeto foi desenvolvido a fins academicos para simular uma movimentação";
+    desconsole += " de registro."
+    console.log(desconsole);
+
